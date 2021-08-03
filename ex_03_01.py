7ba9f0b8c7da4dd1f2d605be456c6868d160295b
dayun0511 <77598522+dayun0511@users.noreply.github.com>
2021-08-03 23:56:47 +0900

    Update ex_03_01.py

--- ex_03_01.py
+++ ex_03_01.py
@@ -3,6 +3,7 @@ rate = input("Enter Rate: ")
 #string 타입으로 입력받은 숫자를 float 타입으로 변환
 fh = float(hour)
 fr = float(rate)
+#40시간을 초과할 경우 초과분에 대해 1.5배의 수당을 지급하는 경우를 조건문으로 작성
 if fh > 40 :
     regular = fh * fr
     overtime = (fh - 40) * (fr * 0.5)
