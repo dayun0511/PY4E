b9004006acccb87ad8953bae178dd871d2dd3a44
dayun0511 <77598522+dayun0511@users.noreply.github.com>
2021-08-04 23:40:06 +0900

    Update ex_04_06.py

--- ex_04_06.py
+++ ex_04_06.py
@@ -1,4 +1,6 @@
+# 시급계산 함수를 정의
 def computepay(hours, rate) :
+    # 함수 안의 매개변수 명을 유의할 것
     # print("In Computepay", hours, rate)
     if hours > 40 :
         regular = hours * rate
@@ -7,12 +9,17 @@ def computepay(hours, rate) :
     else:
         pay = hours * rate
     # print("Returning", pay)
+    # 여전히 함수 안에 있을 때 이 함수의 반환값을 의미한다.
     return pay
 
+# 1) 먼저 입력값을 받아 string 타입을 float 타입으로 변환한다.
 hour = input("Enter Hours: ")
 rate = input("Enter Rate: ")
 fh = float(hour)
 fr = float(rate)
+
+''' 2) computepay 함수를 호출하여 fh의 값을 hours로 전달, fr의 값을 rate로 전달한다.
+pay 변수가 어떤 값이든 관계없이 xp에 할당된다'''
 
 xp = computepay(fh, fr)
 
